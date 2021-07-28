04f2820e865fc2b59fc5c6728942cf71d6e54583
Reid <Reid.Finn@yahoo.com>
2021-07-28 16:10:16 -0500

    beginning login component

---
+++ src/Login.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Login.css";
+import { Link, useHistory } from "react-router-dom";
+import { useState } from "react";
+
+function Login() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Login
+
